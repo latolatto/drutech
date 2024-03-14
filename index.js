a67235a59480df97a8d3b1920a1d3a774929542d
Laz <latolatto16@gmail.com>
2024-03-14 06:26:43 +0100

    responsiveness main page

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.getElementById("myButton").onclick = function () {
 
 
 $(window).on('scroll', function(e) {
+  // if ($(window).scrollTop() >= ($(".counter").offset().top - ($(window).height()))) {
   if ($(window).scrollTop() >= ($(".counter").offset().top - ($(window).height()))) {
     if (!$(".counter").hasClass("animated")) {
       $('.counter-count').each(function () {
@@ -30,6 +31,9 @@ $(window).on('scroll', function(e) {
       $(".counter").addClass("animated");
     }
   }
+  console.log("Window Scroll Top:", $(window).scrollTop());
+console.log("Counter Offset Top:", $(".counter").offset().top);
+
 });
 
 $(".album-see-more").on("click",function(){
@@ -42,5 +46,56 @@ $(function() {
     $('.imagepreview').attr('src', $(this).find('img').attr('src'));
     $('#imagemodal').modal('show');   
   });		
+});
+
+$('.pop').on('click', function () {
+  // Get the item name from the data attribute
+  var itemName = $(this).data('item-name');
+  
+  // Update the modal title with the item name
+  $('#itemName').text(itemName);
+
+  // Update the modal image source
+  $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+
+  // Show the modal
+  $('#myModal').modal('show');
+});
+
+
+$('#searchForm').submit(function (e) {
+  e.preventDefault(); // Prevent the default form submission
+
+  // Get the search term from the input field
+  var searchTerm = $('#exampleDataList').val().trim().toLowerCase();
+
+  // Remove any existing search result classes
+  $('.album-row-catalogue .card').removeClass('search-result');
+
+  // Perform the search and mark matching cards
+  if (searchTerm !== '') {
+    $('.album-row-catalogue .card').each(function () {
+      var productName = $(this).find('.card-body').text().toLowerCase();
+
+      // Check if the product matches the search term
+      if (productName.includes(searchTerm)) {
+        $(this).addClass('search-result');
+      }
+    });
+  }
+
+  // Show all cards if search term is empty or whitespace
+  if (!searchTerm) {
+    $('.album-row-catalogue .card').addClass('search-result');
+  }
+
+  // Hide non-matching cards
+  $('.album-row-catalogue .card').each(function () {
+    if (!$(this).hasClass('search-result')) {
+      $(this).hide();
+    } else {
+      $(this).show(); // Ensure matching cards are visible
+    }
+  });
 });
 
