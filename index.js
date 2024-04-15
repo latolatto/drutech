eb7e00523b15b1172102d79f712021a9c84dc931
Laz <latolatto16@gmail.com>
2024-04-15 15:21:09 +0200

    our work

--- index.js
+++ index.js
@@ -132,3 +132,22 @@ window.addEventListener("load", initSlider);
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+
+// Get all buttons with class 'myButton'
+var buttons = document.querySelectorAll('.btn');
+
+// Loop through each button and add a click event listener
+buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        // Add the 'clicked' class to the button
+        this.classList.add('clicked');
+
+        // Remove the 'clicked' class after 100 milliseconds (adjust as needed)
+        var that = this;
+        setTimeout(function() {
+            that.classList.remove('clicked');
+        }, 100);
+    });
+});
